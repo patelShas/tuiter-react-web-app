0f4632a406eeceaefc521bb14daaa89596ed7b1e
patelShas <patel.shas@northeastern.edu>
2022-11-20 12:43:16 -0500

    logging env variable so i can see netlify stuff

--- src/tuiter/home/index.js
+++ src/tuiter/home/index.js
@@ -1,7 +1,10 @@
 import TuitsList from "../tuits/tuits-list";
 import WhatsHappening from "./whats-happening";
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 const HomeCompenent = () => {
+    console.log(TUITS_API)
     return (
             <>
                 <h4>Home</h4>
